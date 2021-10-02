9e2ab2463be79ec70571121950b6c5f50b546a6e
AKPanesar <88969540+akpanesar@users.noreply.github.com>
2021-10-02 17:43:02 +0100

    further JS updated

--- src/script.js
+++ src/script.js
@@ -1,35 +1,49 @@
-let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-let months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sept",
-  "Oct",
-  "Nov",
-  "Dec",
-];
+function formatDate(timestamp) {
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-let now = new Date();
-let day = days[now.getDay()];
-let month = months[now.getMonth()];
-let hour = now.getHours();
-let minute = now.getMinutes();
-let date = now.getDate();
+  let now = new Date(timestamp);
+  let day = days[now.getDay()];
+  let month = months[now.getMonth()];
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let date = now.getDate();
 
-if (hour < 10) {
-  hour = `0${hour}`;
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `Last updated ${day}, ${date} ${month} ${hour}:${minute}`;
 }
-if (minute < 10) {
-  minute = `0${minute}`;
-}
 
-let currentDate = document.querySelector("#today");
-currentDate.innerHTML = `Last Updated ${day}, ${date} ${month} ${hour}:${minute}`;
+function formatTime(timestamp) {
+  let now = new Date(timestamp);
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let date = now.getDate();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `${hour}:${minute}`;
+}
 
 function showWeather(response) {
   console.log(response.data);
@@ -40,10 +54,8 @@ function showWeather(response) {
   let description = response.data.weather[0].description;
   let hTemp = Math.round(response.data.main.temp_max);
   let windSpeed = Math.round(response.data.wind.speed);
-  let sunRise = response.data.sys.sunrise;
   let lTemp = Math.round(response.data.main.temp_min);
   let humidity = response.data.main.humidity;
-  let sunSet = response.data.sys.sunset;
   let weather_icon = response.data.weather[0].icon;
 
   let cityElement = document.querySelector("#citySearch");
@@ -57,6 +69,7 @@ function showWeather(response) {
   let windElement = document.querySelector("#windSpeed");
   let sunsetElement = document.querySelector("#sunset");
   let weatherIcon = document.querySelector("#weatherIcon");
+  let currentDate = document.querySelector("#today");
 
   cityElement.innerHTML = `${city}, ${country}`;
   temperatureElement.innerHTML = `${tempC}`;
@@ -64,29 +77,32 @@ function showWeather(response) {
   descriptionElement.innerHTML = `${description}`;
   highTempElement.innerHTML = `${hTemp}`;
   windElement.innerHTML = `${windSpeed}`;
-  sunriseElement.innerHTML = `${sunRise}`;
+  sunriseElement.innerHTML = formatTime(response.data.sys.sunrise * 1000);
   lowTempElement.innerHTML = `${lTemp}`;
   humidityElement.innerHTML = `${humidity}%`;
-  sunsetElement.innerHTML = `${sunSet}`;
+  sunsetElement.innerHTML = formatTime(response.data.sys.sunset * 1000);
   weatherIcon.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${weather_icon}@2x.png`
   );
+  currentDate.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+function search(city) {
+  let apiKey = "3bf3d898af236340eac60ab5658c130c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showWeather);
 }
 
 function showNewCityWeather(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
-  let searchFromInput = searchInput.value;
-  let apiKey = "3bf3d898af236340eac60ab5658c130c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchFromInput}&units=metric`;
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(showWeather);
+  search(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", showNewCityWeather);
 
-//Challenge 2
 function showLocationTemperature(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(showCurrentPosition);
@@ -100,3 +116,5 @@ function showLocationTemperature(event) {
 }
 let currentLocationButton = document.querySelector(".locationButton");
 currentLocationButton.addEventListener("click", showLocationTemperature);
+
+search("London");
